8abdbcbcdfeae22de8f175e08f5ea8b51301b60f
Albert Arakelyan <albertarakleyan1998@gmail.com>
2024-01-18 21:53:59 +0400

    added home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,15 @@ export default {
       colors: {
         primary: {
           DEFAULT: '#1F2328',
+          light: '#474E54',
+          dark: '#171B1E',
+          darker: '#0E1114',
+        },
+        secondary: {
+          DEFAULT: '#0969DA',
+          light: '#4B9CE6',
+          dark: '#0058A7',
+          darker: '#003D6F',
         },
       },
     },
